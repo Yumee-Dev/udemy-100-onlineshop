e4a541f6ed17dcad6053f8b56d00ac71caefb5ef
Dmitriy Trifonov <yumee.dev@gmail.com>
2022-03-08 16:11:21 +0300

    Set up rendering of basic 401, 404 and 500 pages with included head and header EJS templates

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+
 const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.json({ message: "Hello world!" });
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: false }));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/401', (req, res) => {
+    res.status(401).render('401');
+});
+
+app.get('/404', (req, res) => {
+    res.status(404).render('404');
+});
+
+app.get('/500', (req, res) => {
+    res.status(500).render('500');
 });
 
 console.log('Server is running...');
