67767b620706170fe81f681e5ab8add7fadef93e
Dmitriy Trifonov <yumee.dev@gmail.com>
2022-03-31 14:03:50 +0300

    Add flashing entered data to session and retrieving data from session

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,15 +4,38 @@ const { userCredentialsAreValid } = require('../util/fields-validation');
 const sessionFlash = require('../util/session-flash');
 
 function getSignup(req, res) {
-    res.render('customer/auth/signup');
+    let sessionData = sessionFlash.getSessionData(req);
+
+    if (!sessionData) {
+        sessionData = {
+            email: '',
+            confirmEmail: '',
+            password: '',
+            fullname: '',
+            street: '',
+            postal: '',
+            city: ''
+        };
+    }
+
+    res.render('customer/auth/signup', { inputData: sessionData });
 }
 
 async function signup(req, res, next) {
-    const enteredData = {}; // 9:51 Flashing errors
+    const enteredData = {
+        email: req.body.email,
+        confirmEmail: req.body['confirm-email'],
+        password: req.body.password,
+        fullname: req.body.fullname,
+        street: req.body.street,
+        postal: req.body.postal,
+        city: req.body.city
+    };
 
     if (!userCredentialsAreValid(req.body.email, req.body['confirm-email'], req.body.password, req.body.street, req.body.postal, req.body.city)) {
         sessionFlash.flashDataToSession(req, {
-            errorMessage: 'Please check your input'
+            errorMessage: 'Please check your input',
+            ...enteredData
         }, () => res.redirect('/signup'));
         return;
     }
@@ -26,14 +49,16 @@ async function signup(req, res, next) {
         req.body.city
     );
 
-
     // manual error handling is necessary
     // because error handling middleware doesn't catch async errors
     try {
         const existsAlready = await user.existsAlready();
 
         if (existsAlready) {
-            res.redirect('/signup');
+            sessionFlash.flashDataToSession(req, {
+                errorMessage: 'User exists already! Try logging in instead!',
+                ...enteredData
+            }, () => res.redirect('/signup'));
             return;
         }
 
@@ -47,7 +72,16 @@ async function signup(req, res, next) {
 }
 
 function getLogin(req, res) {
-    res.render('customer/auth/login');
+    let sessionData = sessionFlash.getSessionData(req);
+
+    if (!sessionData) {
+        sessionData = {
+            email: '',
+            password: ''
+        };
+    }
+
+    res.render('customer/auth/login', { inputData: sessionData });
 }
 
 async function login(req, res, next) {
@@ -61,14 +95,22 @@ async function login(req, res, next) {
     }
 
     if (!existingUser) {
-        res.redirect('/login');
+        sessionFlash.flashDataToSession(req, {
+            errorMessage: 'Invalid credentials! Please check your e-mail and password!',
+            email: user.email,
+            password: user.password
+        }, () => res.redirect('/login'));
         return;
     }
 
     const passwordIsCorrect = await user.hasMatchingPassword(existingUser.password);
 
     if (!passwordIsCorrect) {
-        res.redirect('/login');
+        sessionFlash.flashDataToSession(req, {
+            errorMessage: 'Invalid credentials! Please check your e-mail and password!',
+            email: user.email,
+            password: user.password
+        }, () => res.redirect('/login'));
         return;
     }
 
