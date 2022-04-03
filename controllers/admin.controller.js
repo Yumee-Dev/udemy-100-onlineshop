1ca227133b3dac886d0beda61030d38643fced19
Dmitriy Trifonov <yumee.dev@gmail.com>
2022-04-03 12:28:03 +0300

    Add product model and image uploading

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,12 +1,34 @@
-function getProducts(req, res) {
-    res.render('/admin/products/all-products');
+const Product = require('../models/product.model');
+
+async function getProducts(req, res, next) {
+    try {
+        const products = await Product.findAll();
+    } catch (error) {
+        next(error);
+        return;
+    }
+    res.render('/admin/products/all-products', { products: products });
 }
 
-function getNewProduct() {
+function getNewProduct(req, res) {
     res.render('/admin/products/new-product');
 }
 
-function createNewProduct() {}
+async function createNewProduct(req, res, next) {
+    const product = new Product({
+        ...req.body,
+        image: req.file.filename
+    });
+
+    try {
+        await product.save();
+    } catch (error) {
+        next(error);
+        return;
+    }
+
+    res.redirect('/admin/products');
+}
 
 module.exports = {
     getProducts: getProducts,
